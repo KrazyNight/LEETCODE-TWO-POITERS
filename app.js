79180841662d07bd3fa7fcc82764d1e64aba1e17
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-06-03 12:52:46 -0700

    Leetcode 11, look at graph to solve

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ Note that you must do this in-place without making a copy of the array.
 Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
  */
-// /**
+/**
+ * Correct!
+ * Correct!
+ * Correct!
+ * Correct!
+ * Correct!
+ * Correct!
+ * Correct!
+ */
+/**
 var moveZeroes = function(nums) {
     let l = 0;
     for (let r = 0; r < nums.length; r++){
@@ -26,9 +35,33 @@ var moveZeroes = function(nums) {
     }
     
 };
-// */
+*/
 
 
+
+
+
+
+/**
+ * Leetcode: 11. Container With Most Water
+ * 
+You are given an integer array height of length n. 
+There are n vertical lines drawn such that the two endpoints 
+of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, \
+such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+ */
+
+//look at leetcode graph to help solve
+
+/**
+var maxArea = function(height) {
+    
+};
+ */
 
 
 
