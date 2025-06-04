31999d3161bcfae34fe2b2b59dd493ed73fc766a
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-06-04 12:25:12 -0700

    Leetcode 11, set up

--- app.js
+++ app.js
@@ -56,12 +56,33 @@ Return the maximum amount of water a container can store.
  */
 
 //look at leetcode graph to help solve
-
-/**
-var maxArea = function(height) {
-    
-};
+/**Example 1
+    Input: height = [1,8,6,2,5,4,8,3,7]
+    Output: 49
+Explanation: The above vertical lines are represented by 
+array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) 
+the container can contain is 49.
  */
+
+
+ // /**
+//var maxArea = function(height) {
+    //integer array height of length n.
+    // n vertical lines drawn such that the two endpoints 
+    //ith line are (i, 0) and (i, height[i]).
+    //Find two lines that together with the x-axis form a container,
+    // such that the container contains the most water.
+
+    // step 1; maxArea = length * width
+    // 
+    height = [1,8,6,2,5,4,8,3,7]; // comment out later
+
+
+
+
+
+//};
+ // */
 
 
 
