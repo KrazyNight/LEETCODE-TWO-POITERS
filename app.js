531c7d2b7cca463f964d4d9f12670aa384c0cd79
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-06-05 15:26:25 -0700

    LEETCODE: 15. 3Sum

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//Leetcode: 283. Move Zeroes
+//Leetcode: 283. Move Zeroes
+//Leetcode: 283. Move Zeroes
+//Leetcode: 283. Move Zeroes
+//Leetcode: 283. Move Zeroes
+//Leetcode: 283. Move Zeroes
+//Leetcode: 283. Move Zeroes
+//Leetcode: 283. Move Zeroes
+//Leetcode: 283. Move Zeroes
+
 /**
  * Leetcode: 283. Move Zeroes
 Given an integer array nums, 
@@ -40,7 +50,16 @@ var moveZeroes = function(nums) {
 
 
 
-
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
+//Leetcode: 11. Container With Most Water
 
 /**
  * Leetcode: 11. Container With Most Water
@@ -171,6 +190,18 @@ var maxArea = function(height) {
 */
 
 
+//Leetcode: 167. Two Sum II - Input Array Is Sorted
+//Leetcode: 167. Two Sum II - Input Array Is Sorted
+//Leetcode: 167. Two Sum II - Input Array Is Sorted
+//Leetcode: 167. Two Sum II - Input Array Is Sorted
+//Leetcode: 167. Two Sum II - Input Array Is Sorted
+//Leetcode: 167. Two Sum II - Input Array Is Sorted
+//Leetcode: 167. Two Sum II - Input Array Is Sorted
+//Leetcode: 167. Two Sum II - Input Array Is Sorted
+
+
+ 
+
 /**
  * Leetcode: 167. Two Sum II - Input Array Is Sorted
 
@@ -215,6 +246,17 @@ var twoSum = function(numbers, target) {
 //Correct!
 //Correct!
 
+
+
+//LEETCODE: 125. Valid Palindrome
+//LEETCODE: 125. Valid Palindrome
+//LEETCODE: 125. Valid Palindrome
+//LEETCODE: 125. Valid Palindrome
+//LEETCODE: 125. Valid Palindrome
+//LEETCODE: 125. Valid Palindrome
+//LEETCODE: 125. Valid Palindrome
+//LEETCODE: 125. Valid Palindrome
+
 /**
  * LEETCODE: 125. Valid Palindrome
 
@@ -255,3 +297,49 @@ Explanation: "amanaplanacanalpanama" is a palindrome.
     return true;
 };
 */
+
+
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+//*LEETCODE: 15. 3Sum
+
+/**
+ *LEETCODE: 15. 3Sum
+
+Given an integer array nums, 
+return all the triplets [nums[i], nums[j], nums[k]] 
+such that i != j, i != k, and j != k, and
+ nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+ */
+
+
+// /**
+
+
+var threeSum = function(nums) {
+    
+};
+
+// */
