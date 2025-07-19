0714135ad23e14f60bafa24b86f0f4dac0f9ebf8
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-07-18 18:46:57 -0700

    Leetcode 42. Trapping Rain Water Part end

--- app.js
+++ app.js
@@ -1052,7 +1052,29 @@ Output: 9
 
 /**
  var trap = function(height) {
-    
+	let l = 0;
+	let r = height.lenght - 1;
+
+	let leftMax = height[l];
+	let rightMax =  height[r];
+
+	let water = 0;      //this is the ans, water amount
+
+	while (l < r) {
+		if (leftMax <= rightMax) {
+			l++;
+			leftMax = Math.max(leftMax, height[l]);
+			water += leftMax - height[l];
+		} else {
+			r--;
+			rightMax = Math.max(rightMax, height[r]);
+			water += rightMax - height[r];
+		}
+	}
+	return water;
+
+
+
 };
  */
 
